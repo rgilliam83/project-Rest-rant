4d24a391630f7567a84a400e4f164542a763c408
Rashad Gilliam <akklaim83@gmail.com>
2022-05-30 23:49:34 -0700

    changes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT)
