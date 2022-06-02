63963beffcdc53579228102a6a196529c9eab841
Rashad Gilliam <akklaim83@gmail.com>
2022-06-02 00:42:39 -0700

    changes

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,40 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = [ {
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        Cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+    }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+    }]
+    res.render('places/index', { places })
 })
 
 module.exports = router 
+
+
+  function index (data) {
+     let placesFormatted = data.places.map((place) => {
+         return (
+             <div>
+                 <h2>{place.name}</h2>
+                 <img src={place.pic} alt={place.name}></img>
+             </div>
+         )
+     })
+     return (
+         <Def>
+             <main>
+                 <h1>PLACES INDEX PAGE</h1>
+                 {placesFormatted}
+             </main>
+         </Def>
+     )
+  }
